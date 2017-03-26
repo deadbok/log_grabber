770acda67e1e36573b2595ceaf65bf5b1d37ae32
ObliVion <martin.groenholdt@gmail.com>
2017-03-26 16:54:50 +0200

    Add test log data for GUI

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -50,7 +50,7 @@ class MainWindow(QWidget):
         # Create the buttons
         get_button = QPushButton("Get logs")
         # Connect the get config button to the handler.
-        get_button.clicked.connect(self.getConfigClicked)
+        get_button.clicked.connect(self.getLogsClicked)
 
         quit_button = QPushButton("Quit")
         # Close the window on clicking "Quit"
@@ -100,5 +100,13 @@ class MainWindow(QWidget):
         # Show the message box.
         mb.exec()
 
-    def getConfigClicked(self):
-        pass
+    def getLogsClicked(self):
+        """
+        Get the logs from the router and show them in the GUI.
+        """
+        log_data = [
+            ["Apr 11 10:27:25", "router1", "mgd[3606]", "UI_DBASE_LOGIN_EVENT", "User 'barbara' entering configuration mode"],
+            ["Apr 11 10:32:22", "router1", "mgd[3606]", "UI_DBASE_LOGOUT_EVENT", "User 'barbara' exiting configuration mode"],
+            ["Apr 11 11:36:15", "router1", "mgd[3606]", "UI_COMMIT", "User 'root' performed commit: no comment"],
+            ["Apr 11 11:46:37", "router1", "mib2d[2905]", "SNMP_TRAP_LINK_DOWN", "ifIndex 82, ifAdminStatus up(1), ifOperStatus down(2), ifName at-1/0/0"]
+        ]
